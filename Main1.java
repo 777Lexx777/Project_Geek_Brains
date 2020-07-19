57bf77b06cfcd88ff45ff6a5eec8299f7461094c
geekbrains <lex156166@gmail.com>
2020-07-19 23:34:40 +0400

    chenge lesson_2 file Main1

--- Main1.java
+++ Main1.java
@@ -1,10 +1,11 @@
 package geekArray;
 
 import java.util.Arrays;
+//import.Math.min;
 
 public class Main {
-
     public static void main(String[] args) {
+
       /* Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1,
 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;*/
 
@@ -23,8 +24,8 @@ public class Main {
 значениями 0 3 6 9 12 15 18 21;*/
         System.out.print("Решение_2: ");
         int[] arr2 = new int[8];
-        int j=0;
-        for(int i = 0; i < arr2.length; i++, j = j + 3) {
+        int j = 0;
+        for (int i = 0; i < arr2.length; i++, j = j + 3) {
             arr2[i] = j;
         }
         System.out.println(Arrays.toString(arr2));
@@ -38,7 +39,7 @@ public class Main {
         System.out.print(" - ");
 
         for (int i = 0; i < arr.length; i++) {
-            if (arr3[i] < 6 ) arr3[i] = arr3[i]*2;
+            if (arr3[i] < 6) arr3[i] = arr3[i] * 2;
         }
         System.out.println(Arrays.toString(arr3));
 
@@ -46,18 +47,111 @@ public class Main {
 и с помощью цикла(-ов) заполнить его диагональные элементы единицами;;*/
         System.out.print("Решение_4: ");
 
-        int n =5;
-        int[][] arr4 = new int[n][n];
+        int[][] arr4 = new int[2][2];
+        //System.out.println(arr4);
+        System.out.println(" ");
+        for (int a = 0; a < arr4.length; a++) {
+            for (j = 0; j < arr4.length; j++) {
+                if (a == j) arr4[j][j] = 1;
+                System.out.print(arr4[a][j] + " ");
+            }
+            System.out.println(" ");
+        }
+        /*5. ** Задать одномерный массив и найти в нем минимальный и максимальный элементы (без помощи интернета);;*/
+        System.out.print("Решение_5: ");
+        int[] arr5 = {1, 9, 3};
+        int min = arr[0];
+        int index = 0;
+        System.out.print(Arrays.toString(arr5));
+        System.out.print(" - ");
 
-        for (int i = 0; i < n; i++) {
-            for (j = 0; j < n; j++) {
-                if ((i == j) && arr[i] == n) arr[i] = 1;
+        for (int z = 1; z < arr5.length; z++) {
+            if (arr5[z] < min) {
+                min = arr5[z];
+                index = z;
+
+            }
+        }
+        System.out.println(" min " + min);
+        System.out.println("index" + index);
+
+        for (int n = 1; n < arr5.length; n++) {
+            if (arr5[n] > min) {
+                min = arr5[n];
+                index = n;
+                System.out.println(" max " + min);
+                System.out.println("index" + index);
             }
         }
 
-        System.out.println(Arrays.toString(arr4));
+/*6. ** Написать метод, в который передается не пустой одномерный целочисленный
+ массив, метод должен вернуть true, если в массиве есть место,
+ в котором сумма левой и правой части массива равны.
+ Примеры: checkBalance([2, 2, 2, 1, 2, 2, || 10, 1]) → true,
+ checkBalance([1, 1, 1, || 2, 1]) → true, граница показана символами ||,
+  эти символы в массив не входят.;*/
+        System.out.print("Решение_6: ");
+//
+        System.out.print(" - ");
+        System.out.println(checkBalance(new int[]{2, 2, 2, 1, 2, 2, 1, 1}));
+        System.out.println("\nРешение_7: ");
+        modifyArray(new int[]{2, 2, 2}, 5);
     }
+
+        static boolean checkBalance(int[] arr6) {
+          int left = 0, right = 0;
+          for (int i = 0; i < arr6.length + 1; i++) {
+            left = 0;
+            right = 0;
+            for (int j = 0; j < i; j++) {
+                left += arr6[j];
+            }
+            for (int j = i; j < arr6.length; j++) {
+                right += arr6[j];
+            }
+            if (left == right) return true;
+          }
+            return false;
+        }
+
+/*7. ** Написать метод, которому на вход подается одномерный массив и
+число n (может быть положительным, или отрицательным),
+при этом метод должен сместить все элементымассива на n позиций.
+Для усложнения задачи нельзя пользоваться вспомогательными массивами.;*/
+
+
+           static void modifyArray(int[] arr7, int n) {
+            System.out.print("BEFORE: ");
+             for (int i : arr7) {
+             System.out.print(i + " ");
+          }
+           System.out.print("(n = " + n + ")");
+
+          if (n > 0) {
+            for (int i = 0; i < n; i++) {
+            // Right
+              int tmp = arr7[arr7.length - 1];
+               for (int j = arr7.length - 1; j > 0; j--) {
+                arr7[j] = arr7[j - 1];
+               }
+                    arr7[0] = tmp;
+            }
+          } else if (n < 0) {
+            for (int i = 0; i < n * (-1); i++) {
+            // Left
+            int tmp = arr7[0];
+            for (int j = 0; j < arr7.length - 1; j++) {
+                arr7[j] = arr7[j + 1];
+            }
+            arr7[arr7.length - 1] = tmp;
+        }
+      }
+          System.out.print("\nAFTER:  ");
+          for (int i : arr7) {
+        System.out.print(i + " ");
+      }
+    }
+
+
 }
-//===========================================
-//выдает ошибку что не так?
-//+++++++++++++++++++++++++++++++++
+
